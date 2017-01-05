a9509d3814e6e4813790dcf52ed6fe5174868018
Eric Karge <eric.karge@meteogroup.com>
2017-01-05 11:45:31 +0100

    Add static factory methods for convenience

--- src/main/java/org/meteogroup/kafka/envelope/Envelope.java
+++ src/main/java/org/meteogroup/kafka/envelope/Envelope.java
@@ -8,6 +8,14 @@ public class Envelope<T> {
   public final Map<String, String> headers = new HashMap<>();
   public final T payload;
 
+  public static <T> Envelope<T> envelope(Map<String, String> headers, T payload) {
+    return new Envelope<T>(headers, payload);
+  }
+
+  public static <T> Envelope<T> envelope(T payload) {
+    return new Envelope<T>(payload);
+  }
+
   public Envelope(Map<String, String> headers, T payload) {
     this.headers.putAll(headers);
     this.payload = payload;
